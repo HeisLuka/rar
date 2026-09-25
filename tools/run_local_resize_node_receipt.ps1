32069fde7b5d8c6aa51fad22337b7e651d42f9ce
HeisLuka <167722994+HeisLuka@users.noreply.github.com>
2026-09-25 19:50:48 +0500

    EDITOR-RESIZE-01: build isolated producer tooling crate

--- tools/run_local_resize_node_receipt.ps1
+++ tools/run_local_resize_node_receipt.ps1
@@ -17,7 +17,7 @@ $outputPath = [System.IO.Path]::GetFullPath($Output)
 $sourceHash = (Get-FileHash $fixturePath -Algorithm SHA256).Hash.ToLowerInvariant()
 $documentId = [guid]::NewGuid().ToString()
 
-cargo build -p chaptera-desktop --release --features resize-producer --bin chaptera-resize-producer
+cargo build -p chaptera-resize-producer --release
 if ($LASTEXITCODE -ne 0) {
     throw "chaptera-resize-producer release build failed"
 }
